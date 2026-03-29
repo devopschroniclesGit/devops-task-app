f4fb7167c41b8f47dfd4f496565b2ea0ac41ea69
devopschroniclesGit <github@devopschronicles.com>
2026-03-29 18:08:17 +0200

    Fixed db connection for image

--- app/db.py
+++ app/db.py
@@ -1,7 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://devuser:devpass@db:5432/taskdb"
+DB_USER = os.getenv("DB_USER", "devuser")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "devpass")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME", "taskdb")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
