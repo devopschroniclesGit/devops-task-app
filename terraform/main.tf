bc3fe1ff289bfa60c060cee76d14b177175b88d6
devopschroniclesGit <github@devopschronicles.com>
2026-04-02 22:27:03 +0200

    ci-cd: fixed ec2 connectivity

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,14 @@ resource "aws_security_group" "devopschronicles_sg" {
 
   ingress {
     description = "HTTP application traffic"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP application traffic"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
